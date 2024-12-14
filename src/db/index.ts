a0c551c884fe577a7911737f2ffcaacabbd17198
Daniel de Souza Neris <danielneris01@gmail.com>
2024-12-14 15:08:32 +0200

    feat: first commit

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,5 +3,8 @@ import postgres from 'postgres'
 import * as schema from './schema'
 import { env } from '../env'
 
-export const client = postgres(env.DATABASE_URL)
+export const client = postgres(env.DATABASE_URL, {
+  ssl: { rejectUnauthorized: false },
+})
+
 export const db = drizzle(client, { schema })
